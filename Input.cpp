a27856519d4510c9665f0feea5ba63ccc4d8cc2b
kagamiryoma <115062882+kagamiryoma@users.noreply.github.com>
2023-10-10 16:17:20 +0900

    2-2終わり

--- Input.cpp
+++ Input.cpp
@@ -1,17 +1,8 @@
 #include "Input.h"
 
 #include <cassert>
-#include <wrl.h>
-
-#define DIRECTINPUT_VERSION     0x0800   // DirectInputのバージョン指定
-#include <dinput.h>
-
-#pragma comment(lib, "dinput8.lib")
-#pragma comment(lib, "dxguid.lib")
-
 
 using namespace Microsoft::WRL;
-
 void Input::Initialize(HINSTANCE hInstance, HWND hwnd)
 {
     HRESULT result;
@@ -23,7 +14,7 @@ void Input::Initialize(HINSTANCE hInstance, HWND hwnd)
     assert(SUCCEEDED(result));
 
     // キーボードデバイスの生成
-    ComPtr<IDirectInputDevice8> keyboard;
+    //ComPtr<IDirectInputDevice8> keyboard;
     result = directInput->CreateDevice(GUID_SysKeyboard, &keyboard, NULL);
     // 入力データ形式のセット
     result = keyboard->SetDataFormat(&c_dfDIKeyboard); // 標準形式
@@ -36,4 +27,9 @@ void Input::Initialize(HINSTANCE hInstance, HWND hwnd)
 
 void Input::Update()
 {
+    BYTE key[256] = {};
+    // キーボード情報の取得開始
+    keyboard->Acquire();
+    // 全キーの入力状態を取得する
+    keyboard->GetDeviceState(sizeof(key), key);
 }
--- Input.h
+++ Input.h
@@ -1,6 +1,18 @@
 #pragma once
 
 #include <Windows.h>
+
+
+#define DIRECTINPUT_VERSION     0x0800   // DirectInputのバージョン指定
+#include <dinput.h>
+
+#pragma comment(lib, "dinput8.lib")
+#pragma comment(lib, "dxguid.lib")
+
+#include <wrl.h>
+
+//using namespace Microsoft::WRL;
+
 class Input
 {
 public:
@@ -8,5 +20,8 @@ public:
 	void Initialize(HINSTANCE hInstance, HWND hwnd);
 	// 更新
 	void Update();
+private:
+	// キーボードのデバイス
+	Microsoft::WRL::ComPtr<IDirectInputDevice8> keyboard;
 };
 
